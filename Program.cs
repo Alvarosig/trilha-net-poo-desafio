89c7d0019bbeed60a083e49cf050b381b20c49f1
Álvaro Silva Garcia <alvaro_s.g@hotmail.com>
2023-12-07 15:48:01 -0300

    Implementado toda requisição do desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia("9999", "Moto G3", "ABC-123", 16);
+Console.WriteLine(n.Numero);
+n.InstalarAplicativo("Spotify");
+n.Ligar();
+Console.WriteLine("-------");
+
+Iphone i = new Iphone("8888", "Iphone 11", "SSS-000", 64);
+Console.WriteLine(i.Numero);
+i.InstalarAplicativo("Instagram");
+i.ReceberLigacao();
